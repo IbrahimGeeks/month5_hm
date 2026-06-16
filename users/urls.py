9e8670ea76e67163d527d66524f53ab657294eec
Ibrahim_geeks <fourdeltaone100@gmail.com>
2026-06-16 18:04:07 +0600

    .

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register_api_view),
-    path('confirm/', views.confirm_api_view),
-    path('login/', views.login_api_view),
+    path('register/', views.RegisterAPIView.as_view()),
+    path('confirm/', views.ConfirmAPIView.as_view()),
+    path('login/', views.LoginAPIView.as_view()),
 ]
